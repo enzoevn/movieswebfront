c3d6e63fd8b452d3e0d69000321bcdcfe48c3010
Enzo Viacava <viacava.enzo@gmail.com>
2024-11-29 00:52:59 +0000

    Load the movies

--- src/main/java/es/uah/movieswebfront/controller/HomeController.java
+++ src/main/java/es/uah/movieswebfront/controller/HomeController.java
@@ -1,13 +1,25 @@
 package es.uah.movieswebfront.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import es.uah.movieswebfront.model.Movie;
+import es.uah.movieswebfront.service.IMovieService;
 
 @Controller
 public class HomeController
 {
+    @Autowired
+    private IMovieService movieService;
+    
     @GetMapping("/")
-    public String home() {
-        return "index";
+    public String home(Model model) {
+        List<Movie> movies = movieService.getAllMovies();
+        model.addAttribute("movies", movies);
+        return "home"; // Ensure this matches the name of your home.html template
     }
 }
