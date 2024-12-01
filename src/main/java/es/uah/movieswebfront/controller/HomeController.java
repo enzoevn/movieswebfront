1f5054bcce965c88ab19627f10c88b05bf8fe301
Enzo Viacava <viacava.enzo@gmail.com>
2024-12-01 02:52:09 +0000

    add pagination to homecontroller

--- src/main/java/es/uah/movieswebfront/controller/HomeController.java
+++ src/main/java/es/uah/movieswebfront/controller/HomeController.java
@@ -1,13 +1,15 @@
 package es.uah.movieswebfront.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import es.uah.movieswebfront.model.Movie;
+import es.uah.movieswebfront.paginator.PageRender;
 import es.uah.movieswebfront.service.IMovieService;
 
 @Controller
@@ -17,9 +19,11 @@ public class HomeController
     private IMovieService movieService;
     
     @GetMapping("/")
-    public String home(Model model) {
-        List<Movie> movies = movieService.getAllMovies();
-        model.addAttribute("movies", movies);
+    public String home(Model model, @RequestParam(defaultValue = "0") int page) {
+        Page<Movie> moviePage = movieService.getAllMovies(PageRequest.of(page, 3)); // 3 movies per page
+        PageRender<Movie> pageRender = new PageRender<>("/", moviePage);
+        model.addAttribute("movies", moviePage.getContent());
+        model.addAttribute("page", pageRender);
         return "home"; // Ensure this matches the name of your home.html template
     }
 }
