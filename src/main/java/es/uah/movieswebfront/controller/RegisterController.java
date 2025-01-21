e77f131fb841573dc03bf0682f75ab3adaf60323
Enzo Viacava <viacava.enzo@gmail.com>
2025-01-21 16:04:11 +0000

    se agrega login y register

---
+++ src/main/java/es/uah/movieswebfront/controller/RegisterController.java
@@ -0,0 +1,16 @@
+package es.uah.movieswebfront.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RegisterController {
+
+    @GetMapping ("/register")
+    public String register() {
+        return "register";
+    }
+    
+}
+
+
